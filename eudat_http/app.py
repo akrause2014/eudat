01fb652c361d4c31dcf1bbe8a89b99a9f334d20f
Amy Krause <a.krause@epcc.ed.ac.uk>
2016-08-17 12:49:51 +0100

    moved metadata store in separate module

--- eudat_http/app.py
+++ eudat_http/app.py
@@ -6,12 +6,11 @@ curl -X POST --form "file=@/tmp/test.txt" http://127.0.0.1:5000/digitalobjects/e
 
 import os
 import uuid
-import json
 
 from flask import Flask, request, send_from_directory
 from flask_restful import Resource, Api
-from settings import STORAGE_DIR, METADATA_DIR, DATA_DIR
 import digital_objects as d
+import metadata_file_store as md
 
 STATUS_DRAFT = 'draft'
 STATUS_COMMITTED = 'committed'
@@ -22,52 +21,42 @@ app = Flask(__name__)
 api = Api(app)
 
 class DigitalObjects(Resource):
+
     def get(self):
-        result = []
-        for p in d.list_dirs(STORAGE_DIR):
-            result.append({"id": p})
-        return result
+        return d.list_objects()
 
     def post(self):
         object_id = d.create_digital_object()
-        object_dir = os.path.join(STORAGE_DIR, object_id)
-        md_dir = os.path.join(object_dir, METADATA_DIR)
         metadata = request.get_json()
         if not metadata:
             metadata = {}
-        status = STATUS_DRAFT
-        with open(os.path.join(md_dir, 'metadata'), 'w') as f:
-           json.dump(metadata, f)
-        with open(os.path.join(md_dir, 'status'), 'w') as f:
-           f.write(status)
+        md.store_metadata(object_id, metadata)
+        md.set_status(object_id, STATUS_DRAFT)
         return {"id": object_id}
 
 
 class DigitalObject(Resource):
 
     def get(self, object_id):
-        with open(os.path.join(d.get_md_dir(object_id), 'metadata')) as f:
-            metadata = json.load(f)
-        status = d.get_status(object_id)
+        metadata = md.get_metadata(object_id)
+        status = md.get_status(object_id)
         return {"id": object_id, 
                 "metadata": metadata, 
                 "status": status,
-                "files_count": d.count_files(d.get_data_dir(object_id))}
+                "files_count": d.count_entities(object_id)}
 
     def patch(self, object_id):
         body = request.get_json()
         if body is None or 'status' not in body:
             return {'message' : 'Invalid request: Status expected.'}, 400
         new_status = body['status']
-        md_dir = d.get_md_dir(object_id)
-        with open(os.path.join(md_dir, 'status'), 'w') as f:
-           f.write(new_status)
+        md.set_status(object_id, new_status)
         return {"id": object_id, "status": new_status}
 
     def delete(self, object_id):
-        status = d.get_status(object_id)
+        status = md.get_status(object_id)
         if status == STATUS_DRAFT:
-            d.set_status(object_id, STATUS_DELETED)
+            md.set_status(object_id, STATUS_DELETED)
             return '', 204
         else:
             return {'message': 'Digital object is not in draft status'}, 405
@@ -78,27 +67,15 @@ class DigitalEntities(Resource):
 
     def get(self, object_id):
         result = []
-        for p in d.list_files(d.get_data_dir(object_id)):
+        for p in d.list_entities(object_id):
             result.append({"id": p})
         return result
 
     def post(self, object_id):
         datafile = request.files['file']
-        file_name = datafile.filename
         entity_id = str(uuid.uuid4())
         # entity_id = file_name
-        entity_path = os.path.join(d.get_data_dir(object_id), entity_id)
-        datafile.save(entity_path)
-        file_length = os.path.getsize(entity_path)
-        file_hash = d.get_checksum(entity_path)
-        md_path = os.path.join(d.get_md_dir(object_id), "metadata_" + entity_id)
-        entity_md =  {"id": entity_id, 
-                      "name": file_name,
-                      "length": file_length, 
-                      "checksum": file_hash}
-        with open(md_path, 'w') as f:
-            json.dump(entity_md, f)
-        return entity_md
+        return d.create_entity(object_id, entity_id, datafile)
 
 
 class DigitalEntity(Resource):
@@ -107,8 +84,11 @@ class DigitalEntity(Resource):
         return send_from_directory(directory=d.get_data_dir(object_id), filename=entity_id)
 
     def delete(self, object_id, entity_id):
-        os.remove(os.path.join(d.get_data_dir(object_id), entity_id))
-        return '', 204
+        try:
+            os.remove(os.path.join(d.get_data_dir(object_id), entity_id))
+            return '', 204
+        except:
+            return '', 404
 
 
 api.add_resource(DigitalObjects, '/digitalobjects')
--- eudat_http/digital_objects.py
+++ eudat_http/digital_objects.py
@@ -1,43 +1,57 @@
 import os
 import uuid
-import json
 import hashlib
 
-from settings import STORAGE_DIR, METADATA_DIR, DATA_DIR
+from settings import STORAGE_DIR, DATA_DIR
+import metadata_file_store as md
 
 def create_digital_object():
     for i in range(10): # try up to 10 times to create an id
         object_uid = uuid.uuid4()
-        object_dir = os.path.join(STORAGE_DIR, str(object_uid))
+        object_id = str(object_uid)
+        object_dir = os.path.join(STORAGE_DIR, object_id)
         try:
             os.makedirs(object_dir)
-            os.makedirs(os.path.join(object_dir, METADATA_DIR))
             os.makedirs(os.path.join(object_dir, DATA_DIR))
-            return str(object_uid)
+            md.create_object(object_id)
+            return object_id
         except:
             import traceback
             print(traceback.format_exc())
             # if the directory exists or can't be created we try again
             pass
 
-def get_status(object_id):
-    md_dir = get_md_dir(object_id)
-    with open(os.path.join(md_dir, 'status')) as f:
-        return f.read()
+
+def create_entity(object_id, entity_id, datafile):
+    file_name = datafile.filename
+    entity_path = os.path.join(get_data_dir(object_id), entity_id)
+    datafile.save(entity_path)
+    file_length = os.path.getsize(entity_path)
+    file_hash = get_checksum(entity_path)
+    entity_md =  {"id": entity_id, 
+                  "name": file_name,
+                  "length": file_length, 
+                  "checksum": file_hash}
+    md.store_entity_metadata(object_id, entity_id, entity_md)
+    return entity_md
 
 
-def set_status(object_id, status):
-    md_dir = get_md_dir(object_id)
-    with open(os.path.join(md_dir, 'status'), 'w') as f:
-        return f.write(status)
+def list_objects():
+    result = [{"id": p} for p in list_dirs(STORAGE_DIR)]
+    return result
+
+def count_entities(object_id):
+    return count_files(get_data_dir(object_id))
+
+
+def list_entities(object_id):
+    return list_files(get_data_dir(object_id))
+
 
 
 def get_obj_dir(object_id):
     return os.path.join(STORAGE_DIR, object_id)
     
-def get_md_dir(object_id):
-    return os.path.join(get_obj_dir(object_id), METADATA_DIR)
-
 def get_data_dir(object_id):
     return os.path.join(get_obj_dir(object_id), DATA_DIR)
 
@@ -54,6 +68,7 @@ def list_files(a_dir):
 def count_files(a_dir):
     return len([1 for name in os.listdir(a_dir) \
                 if os.path.isfile(os.path.join(a_dir, name))])
+
 
 def get_checksum(file_name):
     BLOCKSIZE = 65536
